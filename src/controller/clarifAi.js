2e9602ba6ee06ceef9a5700cbd731e081869207b
Mohamed Khelif <khelif96@gmail.com>
2017-10-14 18:49:16 -0400

    Began Implementing Clarifai End Point

---
+++ src/controller/clarifAi.js
@@ -0,0 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: process.env.clarifai_api_token
+});
+
+exports.parseImage = (req,res) => {
+  // predict the contents of an image by passing in a url
+// app.models.predict({concept: {id: "GifMe",}}, 'http://www.globalhealthandwealthonline.com/wp-content/uploads/2016/02/happy-person-2.jpg').then(
+// function(response) {
+//   console.log(response);
+//   res.json({Response: response});
+// },
+// function(err) {
+//   console.error(err);
+//   res.json({Error: err});
+// }
+// );
+}
