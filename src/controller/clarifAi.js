def131da01d27e6f3898ae62a7cabce7b19eb455
Mohamed Khelif <khelif96@gmail.com>
2017-10-14 19:08:10 -0400

    Clarifai Base Implmentaion

--- src/controller/clarifAi.js
+++ src/controller/clarifAi.js
@@ -6,14 +6,14 @@ const app = new Clarifai.App({
 
 exports.parseImage = (req,res) => {
   // predict the contents of an image by passing in a url
-// app.models.predict({concept: {id: "GifMe",}}, 'http://www.globalhealthandwealthonline.com/wp-content/uploads/2016/02/happy-person-2.jpg').then(
-// function(response) {
-//   console.log(response);
-//   res.json({Response: response});
-// },
-// function(err) {
-//   console.error(err);
-//   res.json({Error: err});
-// }
-// );
+app.models.predict('GifMe', 'http://www.globalhealthandwealthonline.com/wp-content/uploads/2016/02/happy-person-2.jpg').then(
+function(response) {
+  console.log(response);
+  res.json({Response: response});
+},
+function(err) {
+  console.error(err);
+  res.json({Error: err});
+}
+);
 }
