0bbac6f8822b4e2596c19638f64106ad2362d4c2
itcarrillo <itcarrillo@gmail.com>
2017-10-14 15:23:15 -0400

    Server done

---
+++ src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.set('view engine', 'hbs');
+const publicPath = path.resolve(__dirname, 'public');
+app.use(express.static(publicPath));
+
+//const apiRouter = require('./routes/api.js');
+const viewsRouter = require('./routes/views.js');
+
+//app.use('apiRouter');
+app.use(viewsRouter.router);
+
+app.listen(3000);
