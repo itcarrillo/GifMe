64dfd69053f2a2b43d864d8363909567812cfc69
Mohamed Khelif <khelif96@gmail.com>
2017-10-15 05:09:20 -0400

    implemented new routes

--- src/controller/giphy.js
+++ src/controller/giphy.js
@@ -2,7 +2,7 @@ const apiKey = process.env.giphy_api_token;
 const apiUrl = 'http://api.giphy.com';
 const request = require('request');
 
-function getGif(req, res) {
+function getGifRendered(req, res) {
 	console.log("Test");
 	let output;
 	const query = req.body.string;
@@ -15,9 +15,27 @@ function getGif(req, res) {
 		else {
 			const rando = parseInt(Math.random() * 30);
 			output = JSON.parse(body).data[rando].images.original.url;
-			return res.render('result', {url: output});
+			res.render('result', {url: output});
 		}
 	});
 }
 
-module.exports = {getGif};
+function getGifUrl(req, res) {
+	console.log("Test Url");
+	let output;
+	const query = req.body.string;
+	const url = apiUrl + '/v1/gifs/search?q=' + query + '&api_key=' + apiKey + '&limit=30';
+	request(url, (error, response, body) => {
+		if (error) {
+			console.log('Error');
+			res.send(error);
+		}
+		else {
+			const rando = parseInt(Math.random() * 30);
+			output = JSON.parse(body).data[rando].images.original.url;
+			// console.log(output);
+			res.send(output);
+		}
+	});
+}
+module.exports = {getGifRendered, getGifUrl};
