c600e729f9680b378b3f0aaf39966ba700755762
itcarrillo <itcarrillo@gmail.com>
2017-10-14 20:15:38 -0400

    Api

--- src/controller/giphy.js
+++ src/controller/giphy.js
@@ -1,10 +1,11 @@
-const api_key = process.env.giphy_api_token;
+const apiKey = process.env.giphy_api_token;
+const apiUrl = 'http://api.giphy.com';
 const request = require('request');
 
-
 function handler(req, res) {
-	const query = req.body; 
-	//request('')
+	const query = req.body;
+	const url = apiUrl + '/v1/gifs/search?' + query;
+	const pictureUrl = res.body.url;
 }
 
 module.exports = {handler};
